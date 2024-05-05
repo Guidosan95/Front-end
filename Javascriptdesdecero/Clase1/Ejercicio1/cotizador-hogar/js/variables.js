d8377397e39e24fea2b2f9dc2f611f15bee9a522
Guidosan95 <guidosan95@outlook.com>
2024-05-04 22:03:51 -0300

    modificacion nueva

---
+++ Javascriptdesdecero/Clase1/Ejercicio1/cotizador-hogar/js/variables.js
@@ -0,0 +1,27 @@
+const datosPropiedad = [
+    {tipo:"Casa", fm: 1.009},
+    
+    {tipo:"P.H", fm: 1.005},
+
+    {tipo:"Dto. Edificio", fm: 1.002},
+
+    {tipo:"Barrio Privado", fm: 1.019},
+
+    {tipo:"Oficina", fm: 1.039},
+
+    {tipo:"Local comercial", fm: 1.041},
+
+    {tipo:"Deposito Logistica", fm: 1.092}
+]
+
+const datosUbicacion = [
+    {ubicacion:"CABA", fm: 1.013},
+    {ubicacion:"Tandil", fm: 1.004},
+    {ubicacion:"Costa Atlantica", fm: 1.029},
+    {ubicacion:"Patagonia Argentina", fm: 1.000}
+]
+
+var fmPropiedad
+var fmUbicacion
+const costom2 = 1.16
+let m2= prompt('Ingresa los metros cuadrados de la vivienda')
