aa92bc3eea2da9e53de66265456e9ed567c2027c
Guidosan95 <guidosan95@outlook.com>
2024-05-15 23:55:52 -0300

    nuevo proyecto

--- Pruebas/Prueba-Youtube/js/button.js
+++ Pruebas/Prueba-Youtube/js/button.js
@@ -1,3 +1,17 @@
-const button = document.querySelector('span svg');
+const button = document.querySelector('a#button');
 
-button.innerHTML = '';
+function mostrar(){
+    button.style.transition = '300ms';
+    button.style.color = 'white';
+    button.style.backgroundColor = 'black';
+    button.style.textDecoration= 'none';
+};
+function ocultar(){
+    button.style.transition = '300ms';
+    button.style.color = 'black';
+    button.style.backgroundColor = 'white';
+
+};
+function click(){
+    window.open = "http://www.youtube.com.ar" ;
+};
